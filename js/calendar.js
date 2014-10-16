861d181622c8fe75c9a477b039ae1679f4389259
nowak-mateusz <matej.mistrzu@gmail.com>
2014-10-16 10:53:53 +0200

    kalendarz: zmiana designu by pasował do strony

--- js/calendar.js
+++ js/calendar.js
@@ -76,7 +76,7 @@ $(document).ready(function() {
          var today = new Date();
          if(event.end < today)
          {
-            $(element).find('.fc-content').before($("<img src=\"http://photoandvision.com/CSS/images/cross-red.png\" style=\"position:absolute;z-index:1;max-height:100px;top: -28px;\" /></img>").html(".."));
+            $(element).find('.fc-content').before($("<img src=\"http://photoandvision.com/CSS/images/cross-red.png\" style=\"position:absolute;z-index:1;max-height:100px;top: -20px; margin-left: -40px;\" /></img>").html(".."));
             //$(element).css({'background-color':'#666666','border-color':'rgba(255, 255, 255, 0.0)'});
          }
          else if(   event.start.date() === today.getDate() 
@@ -111,7 +111,77 @@ $(document).ready(function() {
          }
 
       $('#calendar').fullCalendar('option', 'contentHeight', h);
-      }
+      },
+      
+      eventAfterAllRender: function (){
+      
+      var html = $('<div/>').addClass('hexview');
+      var header = $('<div/>').addClass('header');
+      var header2 = $('<div/>').addClass('header bottom');
+      header.append($('<div/>').addClass('dayname').html('pon'));
+      header2.append($('<div/>').addClass('dayname').html('pon'));
+      header.append($('<div/>').addClass('dayname').html('wt'));
+      header2.append($('<div/>').addClass('dayname').html('wt'));
+      header.append($('<div/>').addClass('dayname').html('śr'));
+      header2.append($('<div/>').addClass('dayname').html('śr'));
+      header.append($('<div/>').addClass('dayname').html('czw'));
+      header2.append($('<div/>').addClass('dayname').html('czw'));
+      header.append($('<div/>').addClass('dayname').html('pt'));
+      header2.append($('<div/>').addClass('dayname').html('pt'));
+      header.append($('<div/>').addClass('dayname').html('sb'));
+      header2.append($('<div/>').addClass('dayname').html('sb'));
+      header.append($('<div/>').addClass('dayname').html('ne'));
+      header2.append($('<div/>').addClass('dayname').html('ne'));
+      html.append(header);
+      html.append(header2);
+      
+      $('.fc-content-skeleton').each( function(i){
+        
+        var dayNum =[];
+        $('table > thead tr td', $(this)).each(function (){
+        
+               var date = [];
+               date['date'] = $(this).html();
+            
+            if($(this).hasClass( "fc-other-month" ))
+               date['othermonth'] = true;
+            else
+               date['othermonth'] = false;
+            
+            if($(this).hasClass( "fc-today" ))
+               date['today'] = true;
+            else
+               date['today'] = false;
+            
+            dayNum.push(date);
+          });
+        
+         if(i % 2 == 0)
+            row = $('<div/>').addClass('hex-row');
+         else
+            row = $('<div/>').addClass('hex-row even');
+            
+         $('table > tbody tr td', $(this)).each(function (i){
+            var col =  $('<div/>').addClass('hexagon');
+            if(dayNum[i]['today'])
+               col.addClass('today');
+            if(dayNum[i]['othermonth'])
+               col.addClass('othermonth');
+            var daynum = $('<div/>').addClass('daynum').html(dayNum[i]['date']);
+            var event = $('<div/>').html($(this).html());
+            col.append(daynum);
+            col.append(event);
+            row.append(col);
+          });
+         html.append(row);
+      });
+
+      //alert(html.html());
+      $( ".fc-view-container" ).css({"display": "none"});
+      $(".hexview").replaceWith("");
+      $( ".fc-view-container" ).after(html);
+      $( "#calendar").css({"margin-bottom": "40px"});
+   } 
    });
    
 });
